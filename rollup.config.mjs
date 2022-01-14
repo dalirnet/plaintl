e6a3fbb6572d658840a1528e53e11b35e3c38cbd
Amir Reza Dalir <dalirnet@gmail.com>
2022-01-15 00:19:00 +0330

    add rollup config

---
+++ rollup.config.mjs
@@ -0,0 +1,31 @@
+import { terser } from "rollup-plugin-terser"
+
+export default [
+    {
+        input: "src/index.mjs",
+        external: [
+            "fs",
+            "path",
+            "lodash",
+            "events",
+            "envfile",
+            "telegram",
+            "fs/promises",
+            "telegram/sessions/index.js",
+            "telegram/extensions/index.js",
+        ],
+        plugins: [terser()],
+        output: [
+            {
+                file: "dist/esm/index.mjs",
+                format: "es",
+                compact: true,
+            },
+            {
+                file: "dist/cjs/index.cjs",
+                format: "cjs",
+                compact: true,
+            },
+        ],
+    },
+]
